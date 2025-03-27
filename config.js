8359b3d89a32de0a5dd26174232cc3f7d17f4e41
dfine <104110488+red-riding-hood@users.noreply.github.com>
2025-03-27 15:11:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ var config = {
      */
     imgs: {
         "心爱的小可爱彬彬小朋友": "./imgs/image1.jpg",
-        //"今天是你的生日": "./imgs/birthday.jpg",
+        "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
